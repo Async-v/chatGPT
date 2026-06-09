8e9136bb91dbd76b680d6dd2c4f20ccf8a5fcd81
vikas <vikas301v@gmail.com>
2026-06-09 14:42:41 +0530

    add socket.io for real-time communication

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 require('dotenv').config();
 const app = require('./src/app');
-const connectToDB = require('./src/config/db');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+// const connectToDB = require('./src/config/db');
 
-connectToDB();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {/* option */})
 
-app.listen(3000, ()=>{
+// connectToDB();
+
+io.on("connection", (socket) => {
+  console.log("A user connected")
+
+  socket.on("disconnect", ()=>{
+    console.log("A user disconnected")
+  })
+
+  socket.on("message", ()=>{
+    console.log("Message Received")
+  })
+});
+
+httpServer.listen(3000, ()=>{
     console.log('Server is running on port 3000')
 })
